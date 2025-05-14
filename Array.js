e2ffe12c14effb78e23c53abac588eb6e4d687cc
ToeNaing Win <19toenaingwin@gmail.com>
2025-05-14 23:34:30 +0630

    Add to New Branch

---
+++ Array.js
@@ -0,0 +1,43 @@
+//Array is ordered List
+
+const GG = {
+    game : "Omyji Arena",
+    v5 : "Mlbb",
+    MueGame : "Pubg"
+}
+const arrayname = ["Mg Toe Naing Win",18,"Computer university","Wundwin",GG];
+
+//To know the length  of array
+
+arrayname.length ;
+
+//To Know the last index
+
+arrayname[arrayname.length-1];
+
+//To Add  new thing at the end of Array
+
+arrayname.push("I am new User");
+
+//To Add new thing at the start of Array
+
+arrayname.unshift("I am the First");
+
+//To Delete the last things of the Array
+
+arrayname.pop();
+
+//To delete the first things of the Array
+
+arrayname.shift() ;
+
+//To delete with start point and end point
+
+arrayname.splice(2,2);
+
+//To Know the index of Array
+
+arrayname.indexOf(18);
+
+//Array can add Object
+//Array can add Array
